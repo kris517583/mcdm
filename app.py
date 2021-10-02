064e7a7d222254822605e21b45c6fe4e68a1d2c6
kris517583 <62744801+kris517583@users.noreply.github.com>
2021-10-02 12:04:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import simpful as sf
+from numpy import *
+
+def rank(arr):
+    temp = arange(len(arr))
+    temp.fill(-1)
+    n = len(arr)
+    for i in range(n):
+        index = where(isclose(arr,max(arr)))
+        temp[index[0][0]] = i+1
+        arr[index[0][0]] = -9999
+    return temp
+
 app = Flask(__name__)
 @app.route('/', methods = ['POST'])
 def home():
     if(request.method == 'POST'):
         #data = request.get_json()
         print(request.json)
+        inputs = ["Accountability","Agility","Assurance","Financial","Performance","Security","Usability"]
+        rulesframe = [
+              [["Good","Very_Good"],["Medium","Good"],["Good","Very_Good"],["Medium","Good"],["Good","Very_Good"],["Good","Very_Good"],["Good","Very_Good"]],
+              [["Good","Very_Good"],["Medium","Good"],["Medium","Good"],["Good","Very_Good"],["Good","Very_Good"],["Good","Very_Good"],["Good","Very_Good"]],
+              [["Good","Very_Good"],["Medium","Good"],["Good","Very_Good"],["Medium","Good"],["Good","Very_Good"],["Good","Very_Good"],["Good","Very_Good"]],
+              [["Good","Very_Good"],["Medium","Good"],["Medium","Good"],["Very_Low","Very_Low"],["Medium","Good"],["Good","Very_Good"],["Medium","Good"]],
+              [["Good","Very_Good"],["Medium","Good"],["Good","Very_Good"],["Medium","Good"],["Medium","Good"],["Good","Very_Good"],["Medium","Good"]]
+             ]
+        scale = ["Very_Low","Low","Medium","Good","Very_Good"]
+        rate = [1,2,3,4,5]
+
         FS = sf.FuzzySystem()
         S_1 = sf.FuzzySet( points=[[0,0],[0.2,1],[0.4,0]],term="Very_Low")
         S_2 = sf.FuzzySet( points=[[0.2,0],[0.4,1],[0.6,0]],term="Low")
@@ -57,8 +80,8 @@ def home():
         S_35 = sf.FuzzySet( points=[[0.8,0],[0.9,1],[1,0]],term="Very_Good" )
         FS.add_linguistic_variable("Usability", sf.LinguisticVariable( [S_31, S_32, S_33,S_34,S_35] ))
 
-        FS.set_output_function("Very_Bad","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability)")
-        FS.set_output_function("Bad","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability + 0.25)")
+        FS.set_output_function("Very_Low","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability)")
+        FS.set_output_function("Low","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability + 0.25)")
         FS.set_output_function("Medium","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability+ 0.5)")
         FS.set_output_function("Good","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability +0.75)")
         FS.set_output_function("Very_Good","(0.2*Accountability+0.5*Agility+0.5*Assurance+0.8*Financial+0.7*Performance+0.3*Security+0.2*Usability + 1)")
@@ -69,16 +92,43 @@ def home():
         RULE4 = "IF ((Accountability IS Good) AND (Agility IS Good) AND (Assurance IS Good) AND (Financial IS Good) AND (Performance IS Very_Low) AND (Security IS Good) AND (Usability IS Good))THEN (Out IS Good)"
         RULE5 = "IF ((Accountability IS Very_Good) AND (Agility IS Very_Good) AND (Assurance IS Very_Good) AND (Financial IS Very_Good) AND (Performance IS Very_Good) AND (Security IS Very_Good) AND (Usability IS Very_Good))THEN (Out IS Very_Good)"
 
-        userdef = ""
-        k = request.json["response"]
-        for i in range(len(k)):
-        	if i == len(k)-1 :
-        		userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ")"
-        	else:
-        		userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ") AND "
-        RULE6 = "IF ("+ userdef +") THEN (Out IS Very_Good)"
-        FS.add_rules([RULE1, RULE2, RULE3, RULE4, RULE5, RULE6])
+        FS.add_rules([RULE1, RULE2, RULE3, RULE4, RULE5])
 
+        response = request.json["response"]
+        userinput = []
+        ruleset = set([])
+        userrate = []
+        userdef = ""
+        for i in range(len(response)):
+            userinput.append(response[i]["name"])
+            userrate.append(rate[scale.index(response[i]["rangeVal"])])
+            if i == len(response)-1 :
+                userdef = userdef + "(" + response[i]["name"] + " IS " + response[i]["rangeVal"] + ")"
+            else:
+                userdef = userdef + "(" + response[i]["name"] + " IS " + response[i]["rangeVal"] + ") AND "
+
+        userdef = "IF ("+ userdef +") THEN (Out IS Very_Good)"
+        ruleset.add(userdef)
+        for i in range(len(rulesframe)):
+            for j in range(2):
+                temp = "IF ("
+                relativedecrease = 0
+                for k in range(len(userinput)):
+                    if k == len(userinput) - 1:
+                        temp = temp + "(" + userinput[k] + " IS " + rulesframe[i][inputs.index(userinput[k])][j] + ")"
+                    else:
+                        temp = temp + "(" + userinput[k] + " IS " + rulesframe[i][inputs.index(userinput[k])][j] + ") AND "
+                    rel = userrate[k] - rate[scale.index(rulesframe[i][inputs.index(userinput[k])][j])]
+                    if rel>relativedecrease :
+                        relativedecrease = rel
+                val = scale[4-relativedecrease]
+                temp = temp + ") THEN (Out IS "+ val +")"
+                ruleset.add(temp)
+        for i in ruleset:
+            FS.add_rules([i])
+
+        outs = []
+        data = {}
         FS.set_variable("Accountability",0.98)
         FS.set_variable("Agility",0.7)
         FS.set_variable("Assurance",0.9)
@@ -86,24 +136,86 @@ def home():
         FS.set_variable("Performance",0.9)
         FS.set_variable("Security",0.98)
         FS.set_variable("Usability",0.85)
+        data["CSP1"] = {}
 
-        print(RULE6)
-        data = FS.Sugeno_inference(['Out'])['Out']
-        print (data)
-        return jsonify({'data': data})
+        data["CSP1"]["Out"] = FS.Sugeno_inference(['Out'])['Out']
+        data["CSP1"]["Name"] = "AWS"
+        data["CSP1"]["Rank"] = -1
+        outs.append(data["CSP1"]["Out"])
+
+        FS.set_variable("Accountability",0.95)
+        FS.set_variable("Agility",0.7)
+        FS.set_variable("Assurance",0.675)
+        FS.set_variable("Financial",0.833)
+        FS.set_variable("Performance",0.833)
+        FS.set_variable("Security",0.96)
+        FS.set_variable("Usability",0.9)
+        data["CSP2"] = {}
+
+        data["CSP2"]["Out"] = FS.Sugeno_inference(['Out'])['Out']
+        data["CSP2"]["Name"] = "Azure"
+        data["CSP2"]["Rank"] = -1
+        outs.append(data["CSP2"]["Out"])
+
+        FS.set_variable("Accountability",0.93)
+        FS.set_variable("Agility",0.64)
+        FS.set_variable("Assurance",0.825)
+        FS.set_variable("Financial",0.733)
+        FS.set_variable("Performance",0.833)
+        FS.set_variable("Security",0.97)
+        FS.set_variable("Usability",0.8)
+        data["CSP3"] = {}
+
+        data["CSP3"]["Out"] = FS.Sugeno_inference(['Out'])['Out']
+        data["CSP3"]["Name"] = "Google Cloud"
+        data["CSP3"]["Rank"] = -1
+        outs.append(data["CSP3"]["Out"])
+
+        FS.set_variable("Accountability",0.8)
+        FS.set_variable("Agility",0.66)
+        FS.set_variable("Assurance",0.7)
+        FS.set_variable("Financial",0.167)
+        FS.set_variable("Performance",0.7)
+        FS.set_variable("Security",0.8)
+        FS.set_variable("Usability",0.75)
+        data["CSP4"] = {}
+
+        data["CSP4"]["Out"] = FS.Sugeno_inference(['Out'])['Out']
+        data["CSP4"]["Name"] = "Rackspace"
+        data["CSP4"]["Rank"] = -1
+        outs.append(data["CSP4"]["Out"])
+
+        FS.set_variable("Accountability",0.92)
+        FS.set_variable("Agility",0.68)
+        FS.set_variable("Assurance",0.875)
+        FS.set_variable("Financial",0.667)
+        FS.set_variable("Performance",0.767)
+        FS.set_variable("Security",0.89)
+        FS.set_variable("Usability",0.83)
+        data["CSP5"] = {}
+
+        data["CSP5"]["Out"] = FS.Sugeno_inference(['Out'])['Out']
+        data["CSP5"]["Name"] = "Alibaba"
+        data["CSP5"]["Rank"] = -1
+        outs.append(data["CSP5"]["Out"])
+        outranks = rank(outs)
+        for i in range(len(outranks)):
+            data["CSP"+str(i+1)]["Rank"] = int(outranks[i])
+
+        return jsonify({'response': data})
 
 @app.route('/check', methods = ['POST'])
 def check():
-	if(request.method == "POST"):
-		userdef = ""
-		k = request.json["response"]
-		for i in range(len(k)):
-			if i == len(k)-1 :
-				userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ")"
-			else:
-				userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ") AND "
-		print(userdef)
-		return jsonify({'data': 'hi'})
+    if(request.method == "POST"):
+        userdef = ""
+        k = request.json["response"]
+        for i in range(len(k)):
+            if i == len(k)-1 :
+                userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ")"
+            else:
+                userdef = userdef + "(" + k[i]["name"] + " IS " + k[i]["rangeVal"] + ") AND "
+        print(userdef)
+        return jsonify({'data': 'hi'})
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug = True)
